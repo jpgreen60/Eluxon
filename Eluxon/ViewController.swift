df6db9638c57bebe4bfc5becd0aac0585dd1b23e
John Green <jpgreen@johns-imac.local>
2018-07-03 15:06:46 -0400

    Initial Commit

---
+++ Eluxon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eluxon
+//
+//  Created by John Green on -07-032018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
